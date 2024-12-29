d863f08653913046e305fcf01748612d1c86a45a
Sajid <108050827+ReXiOP@users.noreply.github.com>
2024-12-29 16:11:44 +0600

    home

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client'; // Updated import for React 18+
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { HashRouter } from 'react-router-dom'; // Use HashRouter if using routing
+import reportWebVitals from './reportWebVitals';
 
-// Use createRoot instead of render
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <HashRouter>
+  <React.StrictMode>
     <App />
-  </HashRouter>
+  </React.StrictMode>
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
