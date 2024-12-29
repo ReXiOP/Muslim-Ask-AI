86dfa836e2da19b3891031721601a6d4ed5c064c
Sajid <108050827+ReXiOP@users.noreply.github.com>
2024-12-29 19:03:29 +0600

    seo

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   return (
     <div className="min-h-screen flex flex-col bg-gray-100 dark:bg-gray-900 text-gray-900 dark:text-gray-100 overflow-x-hidden">
       <header className="p-4 flex justify-between items-center shadow-md">
-        <h1 className="text-2xl font-bold">Ask Muslim</h1>
+        <h1 className="text-2xl font-bold">Quran-Assistant</h1>
         <ThemeSwitch />
       </header>
       <main className="flex-grow">
